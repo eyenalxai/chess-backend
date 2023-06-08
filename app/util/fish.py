a90587fa1c1de817e43c8dcb563e3db87113837a
Sax Dhiut <mail@iyxeyl.me>
2023-06-08 12:24:13 +0400

    fix: URGH

--- app/util/fish.py
+++ app/util/fish.py
@@ -1,15 +1,7 @@
-import subprocess
-
 from stockfish import Stockfish
 
 
-def get_executable_path(*, executable: str) -> str:
-    path = subprocess.check_output(["which", executable])
-    print(path.decode().strip())
-    return path.decode().strip()
-
-
-fish = Stockfish(path=get_executable_path(executable="stockfish"))
+fish = Stockfish(path="/usr/games/stockfish")
 fish.set_skill_level(20)
 
 

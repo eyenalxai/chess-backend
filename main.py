e4ba0c0134cda095e19e65cca1daf6931921f581
Sax Dhiut <mail@iyxeyl.me>
2023-06-08 12:32:28 +0400

    fix: URGH #2

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from app.util.settings import api_settings
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[api_settings.allowed_origin],
+    allow_origins=[
+        "https://{allowed_origin}".format(allowed_origin=api_settings.allowed_origin),
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
